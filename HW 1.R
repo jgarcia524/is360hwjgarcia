5f0fae7fbfcb88049bd747096abe4d4a4387ca33
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-01 18:18:29 -0400

    Added hw assignment 1

---
+++ HW 1.R
@@ -0,0 +1,12 @@
+#Homework Assignment 1
+#Joanne Garcia
+
+#Question 1: I am using R version 3.1.1 and RStudio version 0.98.1028.
+#Question 2: I am using PostgreSQL version 9.3.
+#Question 3:
+install.packages("DMwR")
+require(DMwR)
+library(DMwR)
+data(sales)
+head(sales)
+#Question 3 answer: Using the panel in the upper righthand corner, there are 401146 observations of 5 variables.
